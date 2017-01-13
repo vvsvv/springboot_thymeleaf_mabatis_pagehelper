bc3c0b00866aa689def370265c44879d62fdc1a2
wangshenwei@qq.com <wangshenwei@qq.com>
2017-01-13 16:11:54 +0800

    2017年1月13日16:11:49

---
+++ src/main/java/com/example/common/MyBatisConfig.java
@@ -0,0 +1,86 @@
+package com.example.common;
+
+/**
+ * Created by Administrator on 2017/1/11.
+ */
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import com.github.pagehelper.PageHelper;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+/**
+ * springboot集成mybatis的基本入口
+ * 1）创建数据源
+ * 2）创建SqlSessionFactory
+ */
+@Configuration    //该注解类似于spring配置文件
+@MapperScan(basePackages="com.example.mapper")
+public class MyBatisConfig {
+
+    @Autowired
+    private Environment env;
+
+    /**
+     * 创建数据源
+     * @Primary 该注解表示在同一个接口有多个实现类可以注入的时候，默认选择哪一个，而不是让@autowire注解报错
+     */
+    @Bean
+    //@Primary
+    public DataSource getDataSource() throws Exception{
+        Properties props = new Properties();
+        props.put("driverClassName", env.getProperty("jdbc.driverClassName"));
+        props.put("url", env.getProperty("jdbc.url"));
+        props.put("username", env.getProperty("jdbc.username"));
+        props.put("password", env.getProperty("jdbc.password"));
+        return DruidDataSourceFactory.createDataSource(props);
+    }
+
+    /**
+     * 根据数据源创建SqlSessionFactory
+     *
+     */
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource ds) throws Exception{
+        SqlSessionFactoryBean fb = new SqlSessionFactoryBean();
+        fb.setDataSource(ds);//指定数据源(这个必须有，否则报错)
+        //分页插件
+        PageHelper pageHelper = new PageHelper();
+        Properties props = new Properties();
+        props.setProperty("reasonable", "true");
+        props.setProperty("supportMethodsArguments", "true");
+        props.setProperty("returnPageInfo", "check");
+        props.setProperty("params", "count=countSql");
+        pageHelper.setProperties(props);
+        //添加插件
+        fb.setPlugins(new Interceptor[]{pageHelper});
+//        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+//        fb.setConfigLocation(resolver.getResource("classpath:/mybatis-config.xml"));
+//        fb.setMapperLocations(resolver.getResources("classpath:/mapper/*.xml"));
+
+        //下边两句仅仅用于*.xml文件，如果整个持久层操作不需要使用到xml文件的话（只用注解就可以搞定），则不加
+        fb.setTypeAliasesPackage(env.getProperty("mybatis.typeAliasesPackage"));//指定基包
+        fb.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(env.getProperty("mybatis.mapperLocations")));//指定xml文件位置
+
+        return fb.getObject();
+    }
+
+}
